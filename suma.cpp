27703bd6233c3162fb7b5f536f9b13594ded34ec
Kevin Cedeno <kevin.cedeno@utelvt.edu.ec>
2022-09-23 15:24:10 -0500

    Segunda Version

--- suma.cpp
+++ suma.cpp
@@ -4,9 +4,11 @@ using namespace std;
 int main()
     {
         float A,B,C;
+	cout<<"Ingrese 2 Numeros A B :";
         cin>>A>>B;
         C=A+B;
+	cout<<"El resultado es :";
         cout<<C;
-        return(0);
+        return 0;
 
     }
